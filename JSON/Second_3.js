283d14b7e2ca98c948327ed0b82c88b43a3d0ec4
Shrey Rakholiya <148771089+shrey2126@users.noreply.github.com>
2025-03-20 12:22:53 +0530

    Update Second_3.js

--- JSON/Second_3.js
+++ JSON/Second_3.js
@@ -1,3 +1,17 @@
-const a={
-  
+var obj={
+    'A':'LJU',
+    'B':['CSE','IT','CE'],
+    'C':[{
+        'D':'Hi',
+        'E':['are',4, {'F':['semester','we']}]
+    }],
+    'G':{
+        'H':'students',
+        'I':['of','!']
+    },
+    'J':[{
+        'K':'python',
+        'L':'Branch'
+    },'FSD2']
 }
+console.log(`${obj.C[0].D}${obj.G.I[1]} ${obj.C[0].E[2].F[1]} ${obj.C[0].E[0]} ${obj.G.H} ${obj.G.I[0]} ${obj.C[0].E[2].F[0]} ${obj.C[0].E[1]} ${obj.G.I[0]} ${obj.B[2]} ${obj.J[0].L}`);
