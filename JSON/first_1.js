9eff86f72b8f9fba84fc10272358ae7e23da097b
Shrey Rakholiya <148771089+shrey2126@users.noreply.github.com>
2025-03-20 12:12:51 +0530

    Update first_1.js

--- JSON/first_1.js
+++ JSON/first_1.js
@@ -1,1 +1,20 @@
+var student={
+    "firstname":"xyz",
+    "lastname":"MNC",
+    "ID":"stoo1"
+}
+console.log(student.firstname)
+console.log(student)
+
+
+// Parse Method
+let obj='{"var1":"LJ","var2":"University"}'
+var obj1=JSON.parse(obj)
+console.log(obj1.var1)
+console.log(obj1.var2)
+
+//stringify() Method
+let o1='{"var1":"LJ","var2":"University"}'
+var obj1=JSON.stringify(o1)
+console.log(obj1)
 
