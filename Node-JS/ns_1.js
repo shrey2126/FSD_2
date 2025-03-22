ad6eaf6e79b1d88cb9b609bf61e32b9fbb9c217c
Shrey Rakholiya <148771089+shrey2126@users.noreply.github.com>
2025-03-22 09:53:54 +0530

    Update ns_1.js

--- Node-JS/ns_1.js
+++ Node-JS/ns_1.js
@@ -1,1 +1,31 @@
-Node
+//write  a script to initialize 2 variables and increment b oth the variable each time
+        //  and display the addition of both the variables at interval of 1sec
+
+        let a = 1;
+        let b = 2;
+
+        setInterval(function() {
+        const sum = a + b;
+        console.log(sum);
+        a++;
+        b++;
+        }, 1000); 
+
+//write a script to print "LJU" in the interval of  1sec
+
+        setInterval(function(){
+            console.log("LJU")
+        },1000)
+
+
+
+
+
+
+    
+
+
+
+
+
+
