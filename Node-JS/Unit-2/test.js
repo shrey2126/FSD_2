c8d8285234a10b7c67d1994703eb7e6ec6a90b20
Shrey Rakholiya <148771089+shrey2126@users.noreply.github.com>
2025-04-01 10:42:03 +0530

    Update test.js

--- Node-JS/Unit-2/test.js
+++ Node-JS/Unit-2/test.js
@@ -1,1 +1,22 @@
+//Own Module
 
+const add=(a,b)=>
+{
+    return (a+b);
+}
+
+const sub=(a,b)=>
+{
+    if(a>b){
+        return(a-b);
+    }
+    else{
+        return("No sum +ve sum possible")
+    }
+}
+//module.exports=add,sub; // This becomes confusing to fetch
+module.exports.a=add;
+module.exports.s=sub;
+
+//Another Method
+module.exports={add,sub};
