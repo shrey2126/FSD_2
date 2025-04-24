6c44814fea25ba8247ca46477846f538b45c9c53
Shrey Rakholiya <148771089+shrey2126@users.noreply.github.com>
2025-04-24 11:41:30 +0530

    Update publicaccess.js

--- Express_JS/publicaccess.js
+++ Express_JS/publicaccess.js
@@ -1,6 +1,7 @@
 var exp=require('express');
 var app=exp();
 
-app.use(exp.static('public'))
+//app.use(exp.static('public'))  //it searches for index.html
+app.use(exp.static('public',{index:'home.html'}))
 app.listen(5011);
 //accessoing different files from a static type folder
